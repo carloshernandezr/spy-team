b88b7d0a0ee1a3b119cfb94ac1ddd8bf7d977ee5
Carlos Hernandez <carlos@devcodify.com>
2020-02-08 23:40:42 -0700

    create file for test

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //.
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "                   ",
+      message: "What is your user name?",
+      name: "username"
+    },
+    {
+      type: "",
+      message: "",
+      name: ""
+    },
+
+  ])
+  .then(function(response) {
+
+    if (response.confirm === response.password) {
+      console.log("Success!");
+    }
+    else {
+      console.log(" !");
+    }
+  });
+
+
+
